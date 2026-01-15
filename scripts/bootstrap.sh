b9debc3df559fbd195f5aec73d272bc8ad56ea88
Leonardo Amaral <leonardo_amaral98@hotmail.com>
2026-01-15 19:59:37 -0300

    feat(update): improve server update workflow

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 set -e
 
-SRC="/srv/hytale/Server"
-DST="/srv/data"
+install_hytale() {
+    curl https://downloader.hytale.com/hytale-downloader.zip -o /tmp/hytale-downloader.zip
+    unzip /tmp/hytale-downloader.zip -d /tmp/hytale/
+    rm /tmp/hytale-downloader.zip
+    /tmp/hytale/hytale-downloader-linux-amd64 -download-path /tmp/game.zip
+    rm -rf /tmp/hytale
+    unzip /tmp/game.zip -d /srv/hytale/
+    rm /tmp/game.zip
+}
 
-mkdir -p "$DST"
+setup_volume() {
+    SRC="/srv/hytale/Server"
+    DST="/srv/data"
 
-for f in "$SRC"/*; do
-    rm -rf "$DST/$(basename "$f")"
-    mv "$f" "$DST"/
-done
+    mkdir -p "$DST"
+
+    for f in "$SRC"/*; do
+        rm -rf "$DST/$(basename "$f")"
+        mv "$f" "$DST"/
+    done
+}
+
+install_hytale
+setup_volume
 
 exec "$@"
